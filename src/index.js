e6c338ba1b3350319a830fd0aa146035fee08174
Shineast <canfrog@naver.com>
2023-09-19 23:59:10 +0900

    Learn Basic ThreeJS Structure

--- src/index.js
+++ src/index.js
@@ -2,8 +2,31 @@ import * as THREE from 'three'
 import { WEBGL } from './webgl'
 
 if (WEBGL.isWebGLAvailable()) {
-  // 이 곳에 코드 작성
-  console.log(THREE)
+// 장면
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x004fff)
+
+// 카메라 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// 캔버스
+const canvas = document.querySelector('#three')
+
+// 렌더러 추가
+const renderer = new THREE.WebGLRenderer({canvas});
+// renderer.setSize(window.innerWidth, window.innerHeight);
+
+function render(time) {
+    time  *= 0.001; 
+    // cube.rotation.x += time;
+    // cube.rotation.y += time;
+
+    renderer.render( scene, camera );
+    requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
+
+
 } else {
   var warning = WEBGL.getWebGLErrorMessage()
   document.body.appendChild(warning)
